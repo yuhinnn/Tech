6cea72b4751194b7efdf2c99213d93d002135b6d
yuhi_deve <yuhinnn@gmail.com>
2016-03-22 22:41:22 +0900

    投稿ページfirst

--- Tech/config/routes.rb
+++ Tech/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/new' =>'notes#new'
+
+ get '/top' =>'home#top'
+ get '/about' =>'home#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
